c40c87128092ed86118b70b62c5482ef155e319b
leehotin <leehotin1234@gmail.com>
2024-06-19 21:04:16 +0800

    create router

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productRouter = require('./routes/product');
+var stockRouter = require('./routes/stock');
+var deliveryOrderRouter = require('./routes/deliveryOrder');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('product',productRouter);
+app.use('/stock',stockRouter);
+app.use('/deliveryOrder',deliveryOrderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
