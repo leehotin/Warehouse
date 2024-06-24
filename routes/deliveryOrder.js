233417c019af1de2ca2f5023f39241f39b2cdc0d
leehotin <leehotin1234@gmail.com>
2024-06-24 16:02:40 +0800

    update list display at school

--- routes/deliveryOrder.js
+++ routes/deliveryOrder.js
@@ -9,7 +9,13 @@ const dbName = "Warehouse_In_Out_System";
 router.get('/', async (req, res, next) =>{
     try{
         await client.connect();
-        let data = await client.db(dbName).collection("delivery_notes").find().toArray();
+
+        //sort by data
+        let sort = {};
+
+        let data = await client.db(dbName).collection("delivery_notes").find({},{
+          projection:{_id:1,delivery_id:1,type:1,company:1,phone:1,delivery_check:1,delivery_user:1,delivery_at:1}
+        }).toArray();
 
         res.render('deliveryOrder/index',{ datas: data });
       }finally{
