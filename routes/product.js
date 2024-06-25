4e52d2d3712ce3f0b50674d1d25b7b8b0bcf2b4a
leehotin <leehotin1234@gmail.com>
2024-06-25 08:57:42 +0800

    update product join stocks code

--- routes/product.js
+++ routes/product.js
@@ -5,6 +5,8 @@ const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient("mongodb://localhost:27017/");
 const dbName = "Warehouse_In_Out_System";
 
+const ObjectId = require('mongodb').ObjectId;
+
 router.get('/', function(req, res, next) {
   
 });
@@ -15,11 +17,24 @@ router.get('/info', async(req,res,next)=>{
         const products = client.db(dbName).collection("products");
         let data = await products.aggregate([ // join table
             {
+                $match: { _id: new ObjectId('667a117da384de34623892b6')} //找出符合條件的products
+            },
+            {
                 $lookup:{
                     from:"stocks", //目標table
                     localField:"stock_id", // 自己的col 
                     foreignField:"_id", // 目標的col
                     as: "stocks"// 取得資料後的名稱
+                }
+            },
+            {
+                $project: {  //顯示格式
+                    _id: 1,
+                    product_id: 1,
+                    name: 1,
+                    count: 1,
+                    stocks: 1,
+                    data: {$concat:[ "$product_id" ," ","$name"]} //合併內容
                 }
             },
             {
